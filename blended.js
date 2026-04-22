efa20b88e0bc6156a3c3cb3df8df65299a5b848d
T.Mabotja <mabatshegofatso842@gmail.com>
2026-04-22 23:07:57 +0200

    adjusted the blended js to include the slider

--- blended.js
+++ blended.js
@@ -7,15 +7,18 @@ let surOut= document.querySelectorAll("input[type='radio']")
 let blendBtn = document.createElement("button")
 blendBtn.textContent= "Blend Now"
 blendBtn.style.backgroundColor= "Green"
-//create a go label
+
+//create a elements
 let go= document.createElement("label")
 go.textContent="Go: "
 document.body.append(go)
 go.after(blendBtn)
+
 //create a font colour label
 let fontColour= document.createElement("label")
 fontColour.textContent="   Font Colour: "
 fontFam.after(fontColour)
+
 //create a select element and populate the option
 let colour= document.createElement("select")
 colour.innerHTML=`
@@ -37,29 +40,59 @@ surOut[0].checked = false
 surOut[1].checked= false
 surOut[2].checked= false
 
+//the slider
+let adjuster= document.createElement("input")
+adjuster.setAttribute("type", "range")
+adjuster.setAttribute("min", "15")
+adjuster.setAttribute("max", "100")
+adjuster.setAttribute("value", "15")
+document.body.append(adjuster)
+
+let fontS= document.createElement("label")
+fontS.textContent= "    Font Size: "
+colour.after(fontS)
+let fontSize = document.createElement("input");
+fontSize.setAttribute("type","number")
+fontSize.setAttribute("placeholder","font size")
+fontSize.setAttribute("value", "16"); 
+fontS.after(fontSize)
+
+//to store the surname before its vallue is cleared when user clicks button
+let currentSurN="";
 //what to do when a blend button is clicked
 blendBtn.addEventListener('click', ()=>{
     outputSec.innerHTML=""
     outputSec.style.fontFamily=`${fontFam.value}`
     outputSec.style.color=`${colour.value}`
-    radioButton(surname.value, outputSec)
+    outputSec.style.fontSize=`${fontSize.value}`
+    currentSurN= surname.value;
+    radioButton(currentSurN, outputSec)
     surname.value=""
 })
+
+//for the adjuster
+adjuster.addEventListener("input", () => {
+    // only update if there is content
+    if (outputSec.innerHTML !== "") {
+        outputSec.innerHTML = "";
+        outputSec.style.fontFamily = fontFam.value;
+        outputSec.style.color = colour.value;
+        console.log(currentSurN)
+        radioButton(currentSurN, outputSec);
+    }
+});
 
 //a function that takes the surname and the div to display the blend depending on the type of the blend the user wants
 function radioButton(name, div){
     //split by each character
-    name = surname.value.split("")
+    let letters = name.split("")
     //display diagonally sequential
     if(surOut[0].checked){
-        surOut[1].checked= false
-        surOut[2].checked= false
-        
-        for (let i = 0; i < name.length; i++) {
+
+        for (let i = 0; i < letters.length; i++) {
            let spanCont= document.createElement("span")
-            let pixels= 15
-            pixels *=(i+1)
-            spanCont.textContent= name[i]
+            let pixels=adjuster.value*(i+1)
+            spanCont.textContent= letters[i]
             spanCont.style.left= `${pixels}px`
             spanCont.style.top= `${pixels}px`
             spanCont.style.position= "absolute"
@@ -69,13 +102,14 @@ function radioButton(name, div){
     }
     //display randomly
     if(surOut[1].checked){
-        surOut[2].checked= false
-        surOut[0].checked= false
-        for (let i = 0; i < name.length; i++) {
+
+        for (let i = 0; i < letters.length; i++) {
             let spanCont= document.createElement("span")
-            spanCont.textContent= name[i]
-            spanCont.style.left= `${Math.floor(Math.random()*300)}px`
-            spanCont.style.top= `${Math.floor(Math.random()*100)}px`
+            spanCont.textContent= letters[i]
+            //15*20=300
+            spanCont.style.left = `${Math.floor(Math.random() * adjuster.value * 20)}px`;
+            //15*6.667=100
+            spanCont.style.top = `${Math.floor(Math.random() * adjuster.value * 6.667)}px`;
             spanCont.style.position= "absolute"
             div.appendChild(spanCont)      
         } 
@@ -83,14 +117,11 @@ function radioButton(name, div){
     }
     //display diagonally in reverse
     if(surOut[2].checked){
-        surOut[1].checked = false
-        surOut[0].checked= false
-        name.reverse()
-        for (let i = 0; i < name.length; i++) {
+        letters.reverse()
+        for (let i = 0; i < letters.length; i++) {
             let spanCont= document.createElement("span")
-            let pixels= 15
-            pixels *=(i+1)
-            spanCont.textContent= name[i]
+            let pixels=adjuster.value*(i+1)
+            spanCont.textContent= letters[i]
             spanCont.style.left= `${pixels}px`
             spanCont.style.top= `${pixels}px`
             spanCont.style.position= "absolute"
